167baea32c616406924d8b35b1a6eb6fb9029db8
saloniig <saloniggoyal@gmail.com>
2020-05-14 11:55:16 +0530

    Error message has been added

--- src/components/Cube.vue
+++ src/components/Cube.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="cube" >
-		Enter No:<input type="number" id="number" ref="number" /><br />
-
+		Enter No:<br />
+	<input type="text" class="textfield" @keypress="isNumber($event)" id="number" ref="number" />
 		<p>
 			<button v-on:click="getcube">Click Me!</button>
 			<br />
@@ -20,6 +20,16 @@ export default {
 		};
 	},
 	methods: {
+		isNumber: function(evt) {
+      evt = (evt) ? evt : window.event;
+      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+        evt.preventDefault();
+		alert('Enter number only');
+      } else {
+        return true;
+      }
+    },
 		getcube: function() {
 			cube = this.$refs.number.value;
 			return (this.result = cube * cube * cube);
